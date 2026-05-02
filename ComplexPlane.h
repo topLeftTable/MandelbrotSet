2bb2efab94ef8b8939c0989e1fa8c6d82a6fbfe7
LoganMD <logan@logandev.org>
2026-05-02 02:15:55 -0700

    added pic and reset max_iter so it doesnt hurt computers anymore

--- ComplexPlane.h
+++ ComplexPlane.h
@@ -6,7 +6,7 @@
 
 using namespace sf;
 
-const unsigned int MAX_ITER = 512;
+const unsigned int MAX_ITER = 64;
 const float BASE_WIDTH = 4.0;
 const float BASE_HEIGHT = 4.0;
 const float BASE_ZOOM = 0.5;
