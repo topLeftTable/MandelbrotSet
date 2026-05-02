9daae943c9d24c7c1f3a2d3c32181ed9a02d0603
Hamgri <lavavibes18@gmail.com>
2026-05-01 21:22:36 -0700

    implemented most of ComplexPlane constructor (again)

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -8,6 +8,18 @@
 ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
 {
 
+	Vector2i pixelSize = {pixelWidth, pixelHeight};
+	m_pixel_size = pixelSize;
+
+	m_aspectRatio = (pixelHeight * 1.0) / (pixelWidth * 1.0);
+
+	m_plane_center = {0, 0};
+
+	m_plane_size = {BASE_WIDTH, BASE_HEIGHT * m_aspectRatio};
+
+	m_zoomCount = 0;
+
+
 }
 
 void ComplexPlane::draw(RenderTarget &target, RenderStates states) const
--- ComplexPlane.h
+++ ComplexPlane.h
@@ -10,6 +10,12 @@
 
 using namespace sf;
 
+//
+const unsigned int MAX_ITER = 64;
+const float BASE_WIDTH = 4.0;
+const float BASE_HEIGHT = 4.0;
+const float BASE_ZOOM = 0.5;
+
 class ComplexPlane : public Drawable
 {
 public:
